d0a3d1232a13be3fe5f81038ceb362ffa9efb93e
itlwy <9011532@qq.com>
2016-08-27 20:27:41 +0800

    sadasdsadassdasdaa啊

--- app/src/main/java/com/example/mac/hclientupload/Main2Activity.java
+++ app/src/main/java/com/example/mac/hclientupload/Main2Activity.java
@@ -13,7 +13,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main2);//aasa
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
